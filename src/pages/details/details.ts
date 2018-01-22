fab82150a60a71a976503a8b24e2c42fd91049a8
Alberto Franco <alberto.franco86@qmail.cuny.edu>
2018-01-22 17:57:00 -0500

    Updating message works

--- src/pages/details/details.ts
+++ src/pages/details/details.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
 import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
 
 /**
@@ -20,7 +20,7 @@ export class DetailsPage {
   key: string = this.navParams.get('key');
   taskRef: AngularFireList<any>;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, fireDatabase: AngularFireDatabase) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, fireDatabase: AngularFireDatabase, public alertCtrl: AlertController) {
     this.taskRef = fireDatabase.list('tasks');
   }
 
@@ -31,6 +31,39 @@ export class DetailsPage {
   deleteTask() {
     this.taskRef.remove(this.key);
     this.navCtrl.pop();
+  }
+
+  editTask() {
+    let prompt = this.alertCtrl.create({
+      title: 'Edit',
+      message: 'Enter the new task info below',
+      inputs: [
+        {
+          name: 'message',
+          placeholder: 'Title'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          handler: data => {
+            console.log('Cancel pressed');
+          }
+        },
+        {
+          text: 'Update',
+          handler: data => {
+            this.setNewMessage(data);
+          }
+        }
+      ]
+    });
+    prompt.present();
+  }
+
+  setNewMessage(data) {
+    this.taskRef.update(this.key, data);
+    this.message = data.message;
   }
 
 }
