ff762ce561490317019005f80c1c43e31d50a1e8
Alberto Franco <alberto.franco86@qmail.cuny.edu>
2018-01-22 17:29:05 -0500

    Can now get key from tasks

--- src/pages/create/create.ts
+++ src/pages/create/create.ts
@@ -26,9 +26,12 @@ export class CreatePage {
 
   createTask(taskTitle: string, taskMessage: string) {
     const taskRef: firebase.database.Reference = firebase.database().ref(`/tasks`);
+    const key = taskRef.push().key
+    console.log(key);
     taskRef.push({
       title: taskTitle,
-      message: taskMessage
+      message: taskMessage,
+      id: key
     })
   }
 
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -13,10 +13,22 @@ import { DetailsPage } from '../details/details';
 })
 export class HomePage {
 
+  tasksRef: AngularFireList<any>;
   tasks: Observable<any[]>;
 
   constructor(public navCtrl: NavController, fireDatabase: AngularFireDatabase) {
-    this.tasks = fireDatabase.list('tasks').valueChanges();
+    // fireDatabase.list('tasks').snapshotChanges().map(actions => {
+    //   return actions.map(action => ({ key: action.key, ...action.payload.val() }));
+    // }).subscribe(items => {
+    //   //console.log(items);
+    //   //this.tasks = items;
+    //   return items.map(item => item.key);
+    // })
+    // this.tasks = fireDatabase.list('tasks').snapshotChanges();
+    this.tasksRef = fireDatabase.list('tasks');
+    this.tasks = this.tasksRef.snapshotChanges().map(changes => {
+      return changes.map(c => ({ key: c.payload.key, ...c.payload.val() }));
+    })
   }
 
   viewDetails(taskInfo) {
